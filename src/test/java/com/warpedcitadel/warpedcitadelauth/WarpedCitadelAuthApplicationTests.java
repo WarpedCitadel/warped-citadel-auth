7380129f228ca2eefe7dd0d416b7fc31e60a5b3f
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-06-24 19:12:22 -0500

    rm default integration test

--- src/test/java/com/warpedcitadel/warpedcitadelauth/WarpedCitadelAuthApplicationTests.java
+++ src/test/java/com/warpedcitadel/warpedcitadelauth/WarpedCitadelAuthApplicationTests.java
@@ -1,13 +1,13 @@
 package com.warpedcitadel.warpedcitadelauth;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WarpedCitadelAuthApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WarpedCitadelAuthApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
