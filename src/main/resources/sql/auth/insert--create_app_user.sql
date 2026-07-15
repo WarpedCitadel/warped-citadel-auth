946e4017e856a3a927b60697bca864da21b0a4de
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-07-14 19:16:06 -0500

    hotfix

--- src/main/resources/sql/auth/insert--create_app_user.sql
+++ src/main/resources/sql/auth/insert--create_app_user.sql
@@ -11,14 +11,14 @@ WITH ins_app_user_cte AS
 ins_email_verification_code_cte AS
 (
 	INSERT INTO wc01.email_verification_token
-	(app_user_id, token, passcode)
+	    (app_user_id, token, passcode)
 	SELECT au.id,
-	?,
-	?
+	    ?,
+	    ?
 	FROM ins_app_user_cte au
-	returning app_user_id,
-	token,
-	passcode
+	    returning app_user_id,
+	    token,
+	    passcode
 )
 SELECT
 token,
